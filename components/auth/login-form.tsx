b97bd36f7cb3c2facb4bc103bc12730cdf66bea7
AlisaMikhaylovna <2322023152@qq.com>
2024-09-18 16:07:00 +0800

    card

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,3 +1,14 @@
-export const LoginForm = () => {
+import { CardWrapper } from "./card-wrapper";
 
+export const LoginForm = () => {
+    return (
+        <CardWrapper
+            headerLabel="Welcome Back"
+            backButtonLabel="Don't have an account?"
+            backButtonHref="/register"
+            showSocial
+        >
+            Login
+        </CardWrapper>
+    );
 };
